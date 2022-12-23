b8d829a4a134245232091232d4aa3f7052f35083
FatlindBytyqi <fb52623@ubt-uni.net>
2022-12-23 22:58:40 +0100

    Added data Annotation

--- SchoolManagmentSystem/Data/Migrations/20221222135623_DepartmentAdd.cs
+++ SchoolManagmentSystem/Data/Migrations/20221222135623_DepartmentAdd.cs
@@ -9,23 +9,25 @@ namespace SchoolManagmentSystem.Data.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "Department",
+                name: "Departments",
                 columns: table => new
                 {
                     ID = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Department", x => x.ID);
+                    table.PrimaryKey("PK_Departments", x => x.ID);
                 });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "Department");
+                name: "Departments");
         }
     }
 }
--- SchoolManagmentSystem/Models/Branch.cs
+++ SchoolManagmentSystem/Models/Branch.cs
@@ -1,11 +1,28 @@
-﻿namespace SchoolManagmentSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace SchoolManagmentSystem.Models
 {
     public class Branch
     {
         public int ID { get; set; }
+
+        [Required]
+        [DisplayName("Name")]
+        [StringLength(50, ErrorMessage = "Name cannot be longer than 50 characters")]
+
         public string Name { get; set; }
 
+        [Required]
+        [DisplayName("Location")]
+        [StringLength(20, ErrorMessage = "Name cannot be longer than 20 characters")]
+
         public string Location { get; set; }
+
+        [Required]
+        [DisplayName("CreatedDate")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}")]
         public DateTime CreatedDate { get; set; }
     }
 }
