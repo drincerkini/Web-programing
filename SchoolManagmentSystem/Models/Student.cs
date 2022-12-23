a52af10b21520b3bb836042103b63af44ef20846
tringahaliti <tringahaliti3@gmail.com>
2022-12-23 21:53:17 +0100

    Added validation for student model

--- SchoolManagmentSystem/Models/Student.cs
+++ SchoolManagmentSystem/Models/Student.cs
@@ -1,11 +1,25 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace SchoolManagmentSystem.Models
 {
 	public class Student
 	{
+		[Key]
 		public int ID { get; set; }
-		public string FirstName { get; set; }
+		[Required]
+		[DisplayName("First Name")]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters")]
+        public string FirstName { get; set; }
+		[Required]
+		[DisplayName("Last Name")]
+        [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters")]
 		public string LastName { get; set; }
+		[Required]
+		[DisplayName("Birth Date")]
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString ="{0:dd-MM-yyyy}")]
 		public DateTime BirthDate { get; set; }
 		public string Address { get; set; }
 		public char Gender { get; set; }
