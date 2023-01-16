508936ef19d91526516c4d96156dd22fa84eedf0
drincerkini <93202832+drincerkini@users.noreply.github.com>
2023-01-16 16:30:01 +0100

    CourseModelAdded

---
+++ SchoolManagmentSystem/Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagmentSystem.Models
+{
+    public class Course
+    {
+        [Key]
+        [Display(Name = "Number")]
+        public int CourseID { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; }
+        [Required]
+        [Range(0, 5)]
+        public int Credits { get; set; }
+    }
+}
