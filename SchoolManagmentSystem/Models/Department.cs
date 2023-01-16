d91ea86591fb5d47758c9142f1b372a55c2e0a5f
FatlindBytyqi <fb52623@ubt-uni.net>
2023-01-16 16:30:02 +0100

    Department Model Added

---
+++ SchoolManagmentSystem/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace SchoolManagmentSystem.Models
+{
+    public class Department
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [StringLength(25, ErrorMessage ="Name cannot be longer than 25 characters")]
+        public string  Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
