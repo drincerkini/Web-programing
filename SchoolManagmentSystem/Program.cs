d49d76a469084608d58cd54c91c0a061ca0b7256
tringahaliti <tringahaliti3@gmail.com>
2023-01-25 14:40:27 +0100

    Fixed Macbook appsettings database

--- SchoolManagmentSystem/Program.cs
+++ SchoolManagmentSystem/Program.cs
@@ -5,7 +5,7 @@ using SchoolManagmentSystem.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("FatlindConnection");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
