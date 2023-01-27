37ddca6738473fe4960cb633deefc44768fe3541
FatlindBytyqi <fb52623@ubt-uni.net>
2023-01-27 03:41:17 +0100

    Fixed Professor/Assistant Relationship

---
+++ SchoolManagmentSystem/Data/Migrations/20230127014120_ProfAssist.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolManagmentSystem.Data.Migrations
+{
+    public partial class ProfAssist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Assistants_ProfessorID",
+                table: "Assistants");
+
+            migrationBuilder.DropColumn(
+                name: "AssistantID",
+                table: "Professors");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assistants_ProfessorID",
+                table: "Assistants",
+                column: "ProfessorID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Assistants_ProfessorID",
+                table: "Assistants");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AssistantID",
+                table: "Professors",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assistants_ProfessorID",
+                table: "Assistants",
+                column: "ProfessorID",
+                unique: true);
+        }
+    }
+}
